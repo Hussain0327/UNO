2853f42ab6c68cdb2a4ac7dcda99444e7d5adf41
Raja Hussain <rjfresh988@gmail.com>
2026-05-13 14:52:11 +0000

    Game

--- Game.java
+++ Game.java
@@ -2,9 +2,15 @@
 import java.util.List;
 
 public class Game {
-    private List<Player> players;
-    private Deck deck;
+    private final List<Player> players;
+    private final Deck deck;
     private Color currentColor;
-    
-    
-}
+    private int direction;
+    private boolean isOver;
+    private Player winner;
+
+    public Game(List<Player> players, Deck deck) {
+        this.pla;
+
+
+ 
